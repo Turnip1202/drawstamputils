b745cba38e98b977fee5367c10b0598074cdf466
Turnip1202 <turnip1202@outlook.com>
2025-08-24 15:22:53 +0000

    Deploy: Update deploy.yml c694cd1a2847c32fd6f63c47a7e081c5947e24a8

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  // 如果你的 GitHub Pages 访问路径是 https://username.github.io/repo-name/
-  // 则 base 应该设置为 '/repo-name/'，例如 '/drawstamputils/'
-  base: '/drawstamputils',  // 这里需要填写你的仓库名称
-})
