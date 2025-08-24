355ca525ff1c934d8d456f0788669cccfa1d4eaf
Turnip1202 <82516087+Turnip1202@users.noreply.github.com>
2025-08-24 23:16:29 +0800

    111

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ export default defineConfig({
   plugins: [vue()],
   // 如果你的 GitHub Pages 访问路径是 https://username.github.io/repo-name/
   // 则 base 应该设置为 '/repo-name/'，例如 '/drawstamputils/'
-  base: '/drawstamputils/',  // 这里需要填写你的仓库名称
-  build: {
-    outDir: "docs"  // 保持输出到 docs 目录
-  }
+  base: '/',  // 这里需要填写你的仓库名称
 })
